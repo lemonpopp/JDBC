cb9ca08371748098b9c79c8521a16eaa521e98fa
jeyoon <jypahn@naver.com>
2026-05-15 17:55:22 +0900

    JDBC 1일차 추가 COMMIT

--- JDBC01/src/com/test01/JDBCTest02_2.java
+++ JDBC01/src/com/test01/JDBCTest02_2.java
@@ -1,10 +1,15 @@
 package com.test01;
 
 public class JDBCTest02_2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//jdbc를 이용하여
+		//department 테이블 전체 조회하여
+		//출력문을 이용하여 확인
+		
+		//EMPLOYEE테이블에서
+		//'해외영업1부'에 근무하는 모든 사원의
+		//평균급여, 가장높은급여, 가장낮은급여, 급여합 조회
+		//출력문을 이용하여 조회
 	}
 
 }
